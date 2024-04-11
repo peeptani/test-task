4b2ab16dd79b1ad8dd63438b1a93c002c6196ed1
Peep Tani <peep.tani@gmail.com>
2024-04-11 22:01:50 +0300

    Added in memory database and minimal API endpoints

---
+++ DataBase/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using test_task.Models;
+
+namespace test_task.DataBase
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<HelpdeskRequest> HelpdeskRequests { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using test_task.Components;
 using test_task.Services;
 using test_task.Interfaces;
+using test_task.DataBase;
+using Microsoft.EntityFrameworkCore;
+using test_task.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddScoped<IHelpdeskRequestService, HelpdeskRequestService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("HelpdeskRequestsDb"));
 
 
 var app = builder.Build();
@@ -27,5 +32,33 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+
+// Minimal API 
+app.MapPost("/api/requests", async (HelpdeskRequest request, AppDbContext db) =>
+{
+    db.HelpdeskRequests.Add(request);
+    await db.SaveChangesAsync();
+    return Results.Created($"/api/requests/{request.Id}", request);
+});
+
+app.MapGet("/api/requests/active", async (AppDbContext db) =>
+{
+    var activeRequests = await db.HelpdeskRequests
+                                  .Where(request => !request.IsResolved)
+                                  .OrderByDescending(request => request.Deadline)
+                                  .ToListAsync();
+    return Results.Ok(activeRequests);
+});
+
+app.MapPost("/api/requests/{id}/resolve", async (int id, AppDbContext db) =>
+{
+    var request = await db.HelpdeskRequests.FindAsync(id);
+    if (request == null) return Results.NotFound();
+
+    request.IsResolved = true;
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
 
 app.Run();
